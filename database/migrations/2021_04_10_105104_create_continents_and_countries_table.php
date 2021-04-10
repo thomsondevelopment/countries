f5b238a54a382c58b65c55d6dc5675d772241029
Tomáš Barna <t.barna64@gmail.com>
2021-04-10 12:56:05 +0200

    migration for tables: countries and continents

---
+++ database/migrations/2021_04_10_105104_create_continents_and_countries_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContinentsAndCountriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::unprepared(file_get_contents(base_path('mysql_countries.sql')));
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('countries');
+        Schema::dropIfExists('continents');
+    }
+}
